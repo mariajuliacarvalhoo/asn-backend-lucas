bd94b5ab5c42fe24b8a09405377f386047221101
paulamespindola <paulamespindola04@gmail.com>
2024-09-13 11:09:49 -0300

    fix: correção do EventoMapper

--- src/main/java/br/com/asn/checkin_api/models/mappers/EventoMapper.java
+++ src/main/java/br/com/asn/checkin_api/models/mappers/EventoMapper.java
@@ -3,16 +3,12 @@ package br.com.asn.checkin_api.models.mappers;
 import br.com.asn.checkin_api.models.Evento;
 import br.com.asn.checkin_api.models.dto.EventoDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+
 
 @Mapper (componentModel = "spring")
 public interface EventoMapper {
 
-    EventoMapper INSTANCE = Mappers.getMapper(EventoMapper.class);
-
     EventoDTO toEventoDTO(Evento evento);
     Evento toEvento(EventoDTO eventoDTO);
-
-
 
 }
