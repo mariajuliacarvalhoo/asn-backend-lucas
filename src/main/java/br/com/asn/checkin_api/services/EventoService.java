d1e83eb8aa99685a664bcb97f23ee8ba9b18e8af
paulamespindola <paulamespindola04@gmail.com>
2024-09-13 11:10:18 -0300

    feat: add EventoService

--- src/main/java/br/com/asn/checkin_api/services/EventoService.java
+++ src/main/java/br/com/asn/checkin_api/services/EventoService.java
@@ -1,2 +1,28 @@
-package br.com.asn.checkin_api.services;public class EventoService {
+package br.com.asn.checkin_api.services;
+
+import br.com.asn.checkin_api.models.Evento;
+import br.com.asn.checkin_api.models.dto.EventoDTO;
+import br.com.asn.checkin_api.models.mappers.EventoMapper;
+import br.com.asn.checkin_api.repositories.EventoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventoService {
+
+    private final EventoMapper eventoMapper;
+    private final EventoRepository eventoRepository;
+
+    public EventoService(EventoRepository eventoRepository, EventoMapper eventoMapper) {
+        this.eventoRepository = eventoRepository;
+        this.eventoMapper = eventoMapper;
+    }
+
+    @Transactional
+    public EventoDTO createEvento(EventoDTO eventoDTO){
+        Evento evento = eventoMapper.toEvento(eventoDTO);
+        evento.setStatus(true);
+        eventoRepository.save(evento);
+        return eventoMapper.toEventoDTO(evento);
+    }
 }
